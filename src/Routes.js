049398d7c438b3852143e6c135e2e1378bba7306
your_username <your_email_address@example.com>
2022-07-05 08:49:17 +0530

    ok

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,12 @@ import { Route, Routes } from "react-router-dom";
 
 import Home from "./Pages/Home";
 import PublishArt from "./Pages/PublishArt";
+import SingleNFT from "./Pages/SingleNFT";
+import FractionalNFT from "./Pages/FractionalNFT";
+import ChooseNFT from "./components/FractionalNFT/ChooseNFT";
+import CreateFraction from "./components/FractionalNFT/CreateFraction";
+import MyFractionalNFT from "./components/FractionalNFT/MyFractionalNFT";
+
 import DetailsPage from "./Pages/DetailsPage";
 import TopSelling from "./Pages/TopSelling";
 import CategoryWiseList from "./Pages/CategoryWiseList";
@@ -15,6 +21,20 @@ class Routing extends React.Component {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path="/publishArt" element={<PublishArt />} />
+        <Route exact path="/nft-mint" element={<SingleNFT />} />
+        <Route exact path="/fractional-nft-mint" element={<FractionalNFT />} />
+        <Route
+          exact
+          path="/choose-fractional-nft-mint"
+          element={<ChooseNFT />}
+        />
+        <Route
+          exact
+          path="/create-fractional-nft-mint"
+          element={<CreateFraction />}
+        />
+        <Route exact path="/my-fractional-nft" element={<MyFractionalNFT />} />
+
         <Route exact path="/details/:tokenId" element={<DetailsPage />} />
         <Route
           exact
--- src/config.js
+++ src/config.js
@@ -1,2 +1,8 @@
 export const EtherscanBaseAPI = `https://api-rinkeby.etherscan.io/api`;
 export const EtherscanAPIKEY = `WCVDU52748WW4F7EKDEDB89HKH41BIA4N2`;
+
+export const RinkebyStorageAddress =
+  "0xE2648EFc2b1705a808057ac381e89A36af49f14b";
+export const RinkebyAuctionAddress =
+  "0xBDbe53A0197928F0D3a82E27eA3e36fe4194D06C";
+export const RinkebyNftAddress = "0x3ed3Cf2eB24d8eC5E85D6Ae02C63A8ddc640D51A";
